94aa2af50fcdb704dad77420ca78570a5c353821
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-05-20 12:40:59 +0300

    Delete integration_test.cpp

--- https:/travis-ci.org/VladimirSuvorov/matrix/integration_test.cpp
+++
@@ -1,6 +0,0 @@
-#include "testing_core.hxx"
-//unique integration test
-TEST(is_homework_correct, is_homework_correct){
-    bool mustBeTrue = is_homework_correct();
-    ASSERT_TRUE(mustBeTrue);
-}
